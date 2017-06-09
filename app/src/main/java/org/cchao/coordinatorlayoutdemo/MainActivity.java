ce3230d2d5fb441824c935f15771f9350a8ed675
shucc <cc@cchao.org>
2017-06-09 16:45:57 +0800

    优化调整

--- app/src/main/java/org/cchao/coordinatorlayoutdemo/MainActivity.java
+++ app/src/main/java/org/cchao/coordinatorlayoutdemo/MainActivity.java
@@ -1,14 +1,12 @@
 package org.cchao.coordinatorlayoutdemo;
 
-import android.os.Build;
+import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.cchao.coordinatorlayoutdemo.widget.StickyGridHeadersGridView;
@@ -26,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private CustomScrollView scrollView;
 
+    private ImageView imgExpend;
+
     private AppBarLayout appBarLayout;
 
     private TestAdapter testAdapter;
@@ -36,9 +36,16 @@ public class MainActivity extends AppCompatActivity {
 
     private List<String> headStr;
 
+    //1dp高度
     private float oneDP;
 
+    private int scrollY = 0;
+
+    //AppBar的高度
     private int appBarLayoutHeight = 0;
+
+    //GridView当前可见第一个item position
+    private int firstVisibleItemNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +56,22 @@ public class MainActivity extends AppCompatActivity {
         textTitle = (TextView) findViewById(R.id.text_title);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         scrollView = (CustomScrollView) findViewById(R.id.scrollView);
+        imgExpend = (ImageView) findViewById(R.id.img_expend);
 
+        //Appbar自动折叠
+        imgExpend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appBarLayout.setExpanded(false, true);
+            }
+        });
 
         oneDP = getResources().getDimension(R.dimen.oneDP);
 
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                scrollY = verticalOffset;
                 if (0 == appBarLayoutHeight) {
                     appBarLayoutHeight = appBarLayout.getHeight();
                 }
@@ -79,12 +95,16 @@ public class MainActivity extends AppCompatActivity {
         gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
-
+                Log.d(TAG, "onScrollStateChanged: " + scrollState + "-->" + firstVisibleItemNow + "-->"
+                        + (appBarLayoutHeight - oneDP * 48) + "-->" + (-scrollY));
+                if (firstVisibleItemNow == 0 && -scrollY <= (appBarLayoutHeight - oneDP * 48)) {
+                    scrollView.setInter(true);
+                }
             }
 
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                Log.d(TAG, "onScroll: " + firstVisibleItem + "->" + visibleItemCount + "->" + totalItemCount);
+                firstVisibleItemNow = firstVisibleItem;
                 if (0 == firstVisibleItem) {
                     scrollView.setInter(true);
                 } else {
